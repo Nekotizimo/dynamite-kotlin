f09ac4d925c5a71282901386d132ea0e25e25079
Kenny Cho <Kenny.Cho@softwire.com>
2024-06-28 16:07:01 +0100

    Added Round Table and water counter

--- dynamite/src/main/java/com/example/dynamite/MyBot.kt
+++ dynamite/src/main/java/com/example/dynamite/MyBot.kt
@@ -10,19 +10,63 @@ import kotlin.math.floor
 import kotlin.math.max
 
 class MyBot : Bot {
+
+    var roundTable = Array(25) {Array(5) {0} }
+
     override fun makeMove(gamestate: Gamestate): Move {
         // Are you debugging?
         // Put a breakpoint in this method to see when we make a move
 
         // W and D first 2 to confuse opponent lol
-        if (gamestate.rounds.size == 0) return Move.W
-        else if (gamestate.rounds.size == 1) return Move.D
+        val numRounds = gamestate.rounds.size
+        if (numRounds == 0) return Move.W
+        else if (numRounds == 1) return Move.D
 
-        val randomNumberBetween0And3 =
-            floor(Math.random() * 3.0).toInt()
-        val possibleMoves = arrayOf(Move.R, Move.P, Move.S)
-        val randomMove = possibleMoves[randomNumberBetween0And3]
+        // Update roundTable
+        updateRoundTable(gamestate.rounds[numRounds - 1], gamestate.rounds[numRounds - 2])
 
+        // Decide whether to follow info from roundTable
+        val prevRound = gamestate.rounds.last()
+        val prevRoundIndex = roundToTableIndex(prevRound)
+        val moveDistribution = roundTable[prevRoundIndex]
+
+        val sampleSize = moveDistribution.sum()
+        if (sampleSize >= 10) {
+            // If >50% D, then probably D after draw
+            if (moveDistribution[4] >= sampleSize / 2) {
+                // Counter with W
+                return Move.W
+            }
+            // See if a particular R/P/S is more frequent
+            val rpsDistribution = moveDistribution.slice(0..2)
+            val rpsSampleSize = rpsDistribution.sum()
+            if (rpsSampleSize >= 10) {
+                var rpsDistributionScaled = rpsDistribution.map{ it.toDouble() / rpsDistribution.sum() }
+//                print(rpsDistributionScaled)
+//                rpsDistributionScaled = rpsDistributionScaled.map{ (it).pow(4) }
+//                rpsDistributionScaled = rpsDistributionScaled.map{ it / rpsDistributionScaled.sum() }
+//                print(" scaled: ")
+//                println(rpsDistributionScaled)
+                if (rpsDistributionScaled.max()!! > 0.37) {
+                    return moveToWin(
+                        intToMove(rpsDistributionScaled.indices.maxBy { rpsDistributionScaled[it] }!!))
+                }
+                if (rpsDistributionScaled.min()!! < 0.29) {
+                    return moveToLose(
+                        intToMove(rpsDistributionScaled.indices.minBy { rpsDistributionScaled[it] }!!))
+                }
+            }
+        }
+
+        // Decide to throw water if dynamite draws many times
+        val numDynamiteDraws = calculateDynamiteDraws(gamestate)
+        val waterProbability = numDynamiteDraws.toDouble().pow(2)  / 100.0
+        val waterRNG = Math.random()
+        if (waterRNG < waterProbability) {
+            return Move.W
+        }
+
+        // Decide if throw dynamite
         val dynamitesPlayed = numberOfDynamitesPlayed(gamestate)
         val (scoreP1, scoreP2) = calculateScores(gamestate)
         val movesUntilEnd = 1000 - max(scoreP1, scoreP2)
@@ -37,17 +81,76 @@ class MyBot : Bot {
         }
 //        println("$roundScoreMultiplier $dynamiteProbability")
 
-//        if (movesUntilEnd == 1) {
-//            logGamestate(gamestate)
-//        }
-
         val dynamiteRNG = Math.random()
 //        println("$dynamiteRNG $dynamiteProbability")
         if (dynamiteRNG < dynamiteProbability) {
             return Move.D
         }
 
+        // Play random move
+        val randomNumberBetween0And3 =
+            floor(Math.random() * 3.0).toInt()
+        val possibleMoves = arrayOf(Move.R, Move.P, Move.S)
+        val randomMove = possibleMoves[randomNumberBetween0And3]
+
         return randomMove
+    }
+
+    private fun calculateDynamiteDraws(gamestate: Gamestate): Int {
+        var num = 0
+        for (round in gamestate.rounds.reversed()) {
+            if (round.p1 == Move.D && round.p2 == Move.D) {
+                num++
+            } else break
+        }
+        return num
+    }
+
+    private fun intToMove(index: Int): Move {
+        return when (index) {
+            0 -> Move.R
+            1 -> Move.P
+            2 -> Move.S
+            3 -> Move.W
+            4 -> Move.D
+            else -> Move.D
+        }
+    }
+    private fun moveToInt(move: Move): Int {
+        return when (move) {
+            Move.R -> 0
+            Move.P -> 1
+            Move.S -> 2
+            Move.W -> 3
+            Move.D -> 4
+        }
+    }
+
+    private fun moveToWin(move: Move): Move {
+        return when(move) {
+            Move.R -> Move.P
+            Move.P -> Move.S
+            Move.S -> Move.R
+            else -> Move.D
+        }
+    }
+
+    private fun moveToLose(move: Move): Move {
+        return when(move) {
+            Move.R -> Move.S
+            Move.P -> Move.R
+            Move.S -> Move.P
+            else -> Move.W
+        }
+    }
+
+    private fun roundToTableIndex(round: Round): Int {
+        return round.p1.ordinal * 5 + round.p2.ordinal
+    }
+
+    private fun updateRoundTable(current: Round, previous: Round) {
+        val index = roundToTableIndex(previous)
+        roundTable[index][current.p2.ordinal]++
     }
 
     private fun calculateRoundScore(gamestate: Gamestate): Int {
